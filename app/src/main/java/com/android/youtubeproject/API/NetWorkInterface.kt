0df3855e8d35c6c029eeed36f3e8c3f3d832cedb
seongssu <loveforggong@gmail.com>
2023-09-25 19:09:25 +0900

    [FEAT] : ⚙️ Home: API 데이터 받아오기(로그찍힘), ViewModel 구현중

--- app/src/main/java/com/android/youtubeproject/API/NetWorkInterface.kt
+++ app/src/main/java/com/android/youtubeproject/API/NetWorkInterface.kt
@@ -1,13 +1,30 @@
 package com.android.youtubeproject.API
 
+import VideoData
+import com.android.youtubeproject.Constants
 import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface NetWorkInterface {
-    @GET("videos")
-    suspend fun get(
+    @GET("videos?key=${Constants.Authorization}")
+    suspend fun getVideo(
+        @Query("part") part: String,
+        @Query("chart") chart: String,
+        @Query("maxResults") maxResults: Int,
+        @Query("videoCategoryId") videoCategoryId: String
+    ): VideoData
+    @GET("videos?key=${Constants.Authorization}")
+    suspend fun getNation(
+        @Query("part") part: String,
+        @Query("chart") chart: String,
+        @Query("maxResults") maxResults: Int,
+        @Query("id") id: String
+    )
+    @GET("videos?key=${Constants.Authorization}")
+    suspend fun getChannel(
         @Query("part") part:String,
-//        @Query("")
-//    @Query("")
-    ) : VideoData
+        @Query("id") id:String,
+        @Query("maxResults") maxResults:Int
+    ) : ChannelData
 }
