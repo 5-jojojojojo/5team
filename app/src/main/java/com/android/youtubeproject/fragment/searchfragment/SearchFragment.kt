90caef233d9be78ba21ea71459a04a027f6682f1
seongssu <loveforggong@gmail.com>
2023-10-02 12:47:58 +0900

    [FEAT] : ⚙️ 검색페이지 앱에 표시(뷰모델+라이브데이터)

--- app/src/main/java/com/android/youtubeproject/fragment/searchfragment/SearchFragment.kt
+++ app/src/main/java/com/android/youtubeproject/fragment/searchfragment/SearchFragment.kt
@@ -1,23 +1,78 @@
 package com.android.youtubeproject.fragment.searchfragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.android.youtubeproject.R
+import com.android.youtubeproject.api.NetWorkClient
+import com.android.youtubeproject.databinding.FragmentSearchBinding
+import com.android.youtubeproject.viewmodel.searchmodel.SearchViewModel
+import com.android.youtubeproject.viewmodel.searchmodel.SearchViewModelFactory
 
 
 class SearchFragment : Fragment() {
-
+    private val binding by lazy { FragmentSearchBinding.inflate(layoutInflater) }
+    private lateinit var searchAdapter : SearchFragmentAdapter
+    private val apiServiceInstance = NetWorkClient.apiService
+    private val searchViewModel : SearchViewModel by viewModels{SearchViewModelFactory(apiServiceInstance)}
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+
+        binding.searchView.apply {
+            isSubmitButtonEnabled = true
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    if(!query.isNullOrEmpty()){
+                        val searchQuery = query.trim()
+                        Log.d("YouTubeProjects", "검색어 : ${searchQuery}")
+                        setUpView()
+                        setupListeners(searchQuery,"2")
+                    }
+                    return false
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    return true
+                }
+
+            })
+        }
+
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeViewModel()
+    }
+    private fun observeViewModel(){
+        searchViewModel.searchResults.observe(viewLifecycleOwner){
+            searchAdapter.items.addAll(it)
+            searchAdapter.notifyDataSetChanged()
+        }
+    }
 
+    private fun setUpView(){
+        searchAdapter = SearchFragmentAdapter(requireContext())
+
+        binding.searchRecyclerView.apply {
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            adapter = searchAdapter
+            setHasFixedSize(true)
+        }
+    }
+
+    private fun setupListeners(q:String,videoCategoryId:String){
+        searchViewModel.SearchServerResults(q,videoCategoryId)
+    }
 }
--- app/src/main/java/com/android/youtubeproject/fragment/searchfragment/SearchFragmentAdapter.kt
+++ app/src/main/java/com/android/youtubeproject/fragment/searchfragment/SearchFragmentAdapter.kt
@@ -1,4 +1,40 @@
 package com.android.youtubeproject.fragment.searchfragment
 
-class SearchFragmentAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.youtubeproject.api.model.ChannelModel
+import com.android.youtubeproject.databinding.SearchitemsBinding
+import com.bumptech.glide.Glide
+
+class SearchFragmentAdapter(private val context:Context):RecyclerView.Adapter<RecyclerView
+.ViewHolder>() {
+    var items = ArrayList<ChannelModel>()
+
+    inner class SearchViewHolder(private val binding:SearchitemsBinding) : RecyclerView
+    .ViewHolder(binding.root){
+        fun bind(items:ChannelModel){
+            binding.apply {
+                SearchName.text = items.title
+                Glide.with(context)
+                    .load(items.url)
+                    .into(SearchProfile)
+            }
+        }
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val binding = SearchitemsBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return SearchViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        holder as SearchViewHolder
+        val items = items[position]
+        holder.bind(items)
+    }
 }
