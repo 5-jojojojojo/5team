f554101f6c489a999da041b6f2998f4c84240273
khjgggg <guswjd7@gmail.com>
2023-10-02 21:57:53 +0900

    [DOCS] : 📚(feature/myvideo): MyPageFunc파일 주석

--- app/src/main/java/com/android/youtubeproject/fragment/myvideofragment/MyPageFunc.kt
+++ app/src/main/java/com/android/youtubeproject/fragment/myvideofragment/MyPageFunc.kt
@@ -15,20 +15,24 @@ import java.lang.Exception
 object MyPageFunc {
     private const val MY_VIDEO = "MY_VIDEO"         //실제 상세 내용
 
+    // YoutubeModel 목록을 SharedPreferences에 저장하는 메서드
     fun testDummy(dataList: ArrayList<YoutubeModel>) {
         SharedPref.setString(App.instance.applicationContext, MY_VIDEO, convertToString(dataList.toMutableList()))
     }
 
+    // YoutubeModel 목록을 SharedPreferences에 저장하는 메서드 (Context를 직접 지정할 수 있음)
     fun testDummy(context: Context, dataList: MutableList<YoutubeModel>) {
         SharedPref.setString(context, MY_VIDEO, convertToString(dataList))
     }
 
+    // YoutubeModel을 내 보관함에 저장하는 메서드
     fun saveVideo(data: YoutubeModel) {
         val context = App.instance.applicationContext
         val prevSaveList = SharedPref.getString(context, MY_VIDEO, "")
         val prevList: MutableList<YoutubeModel> = convertToObject(prevSaveList)
 
         if (prevList.isEmpty()) {
+            // 저장된 목록이 없을 경우 새로운 목록을 생성하고 데이터를 추가
             val saveList = mutableListOf<YoutubeModel>()
             saveList.add(data)
             SharedPref.setString(context, MY_VIDEO, convertToString(saveList))
@@ -36,9 +40,11 @@ object MyPageFunc {
             Toast.makeText(context, "내 보관함에 저장.", Toast.LENGTH_SHORT).show()
         } else {
             if (!prevList.any { it.id == data.id }) {
+                // 동일한 ID를 가진 데이터가 없을 경우 데이터 추가
                 prevList.add(data)
                 Toast.makeText(context, "내 보관함에 저장.", Toast.LENGTH_SHORT).show()
             } else {
+                // 동일한 ID를 가진 데이터가 이미 존재하는 경우
                 Toast.makeText(context, "이미 저장되어 있습니다.", Toast.LENGTH_SHORT).show()
             }
             SharedPref.setString(context, MY_VIDEO, convertToString(prevList))
@@ -46,6 +52,7 @@ object MyPageFunc {
 
     }
 
+    // YoutubeModel을 내 보관함에서 삭제하는 메서드
     fun deleteVideo(data: YoutubeModel) {
         val context = App.instance.applicationContext
         val prevSaveList = SharedPref.getString(context, MY_VIDEO, "")
@@ -53,13 +60,16 @@ object MyPageFunc {
 
         val isRemoved = prevList.removeIf { it.id == data.id }
         if (isRemoved) {
+            // 데이터가 삭제되었을 경우 목록을 업데이트
             SharedPref.setString(context, MY_VIDEO, convertToString(prevList))
             Toast.makeText(context, "내 보관함에서 삭제.", Toast.LENGTH_SHORT).show()
         }
     }
 
+    // 특정 ID를 가진 YoutubeModel이 내 보관함에 존재하는지 확인하는 메서드
     fun isExist(data: YoutubeModel): Boolean = isExist(data.id)
 
+    // 특정 ID를 가진 YoutubeModel이 내 보관함에 존재하는지 확인하는 메서드
     fun isExist(id: String): Boolean {
         val context = App.instance.applicationContext
         val prevSaveList = SharedPref.getString(context, MY_VIDEO, "")
@@ -68,6 +78,7 @@ object MyPageFunc {
         return prevList.any { it.id == id }
     }
 
+    // SharedPreferences에서 저장된 YoutubeModel 목록을 불러오는 메서드
     fun loadVideos(): MutableList<YoutubeModel> {
         val context = App.instance.applicationContext
         val prevSaveList = SharedPref.getString(context, MY_VIDEO, "")
@@ -75,6 +86,7 @@ object MyPageFunc {
         return convertToObject(prevSaveList)
     }
 
+    // YoutubeModel 목록을 JSON 문자열로 변환하는 메서드
     private fun convertToString(dataList: MutableList<YoutubeModel>): String {
         return GsonBuilder()
             .disableHtmlEscaping()
@@ -82,6 +94,7 @@ object MyPageFunc {
             .toJson(dataList)
     }
 
+    // JSON 문자열을 YoutubeModel 목록으로 변환하는 메서드
     private fun convertToObject(jsonStr: String): MutableList<YoutubeModel> {
         return try {
             GsonBuilder().create().fromJson(jsonStr, Array<YoutubeModel>::class.java).toMutableList()
@@ -90,6 +103,7 @@ object MyPageFunc {
         }
     }
 
+    // 주어진 Uri에 대한 지속적인 권한이 있는지 확인하는 메서드
     fun hasPersistedUriPermissions(activity: FragmentActivity, pictureUri: Uri): Boolean {
         val persistedUriPermissions = activity.contentResolver.persistedUriPermissions
         val hasPermission = persistedUriPermissions.any { uriPermission ->
@@ -97,6 +111,7 @@ object MyPageFunc {
         }
 
         if(!hasPermission) {
+            // 권한이 없을 경우 메시지 표시
             Toast.makeText(activity, R.string.has_not_permission_dont_profile_img, Toast.LENGTH_SHORT).show()
         }
         return hasPermission
