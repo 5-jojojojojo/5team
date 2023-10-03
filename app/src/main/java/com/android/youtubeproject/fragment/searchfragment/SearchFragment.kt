1721a20e433e397b091c81e37e4d15767be35ecf
seongssu <loveforggong@gmail.com>
2023-10-03 18:50:12 +0900

    [FEAT] : ⚙️ Search: infinityScroll 기능 구현

--- app/src/main/java/com/android/youtubeproject/fragment/searchfragment/SearchFragment.kt
+++ app/src/main/java/com/android/youtubeproject/fragment/searchfragment/SearchFragment.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.android.youtubeproject.R
 import com.android.youtubeproject.api.NetWorkClient
 import com.android.youtubeproject.databinding.FragmentSearchBinding
+import com.android.youtubeproject.infinityscroll.SearchScrollListener
 import com.android.youtubeproject.viewmodel.searchmodel.SearchViewModel
 import com.android.youtubeproject.viewmodel.searchmodel.SearchViewModelFactory
 
@@ -22,33 +23,33 @@ class SearchFragment : Fragment() {
     private lateinit var searchAdapter : SearchFragmentAdapter
     private val apiServiceInstance = NetWorkClient.apiService
     private val searchViewModel : SearchViewModel by viewModels{SearchViewModelFactory(apiServiceInstance)}
+    var searchQuery:String? = null
+    var search_loading = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        setUpView()
 
         binding.searchView.apply {
             isSubmitButtonEnabled = true
             setOnQueryTextListener(object : SearchView.OnQueryTextListener{
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     if(!query.isNullOrEmpty()){
-                        val searchQuery = query.trim()
+                        searchQuery = query.trim()
                         Log.d("YouTubeProjects", "검색어 : ${searchQuery}")
-                        setUpView()
-                        setupListeners(searchQuery,"2")
+                        setupListeners()
                     }
                     return false
                 }
 
                 override fun onQueryTextChange(newText: String?): Boolean {
-                    return true
+                    return false
                 }
 
             })
         }
-
-
         return binding.root
     }
 
@@ -57,9 +58,16 @@ class SearchFragment : Fragment() {
         observeViewModel()
     }
     private fun observeViewModel(){
-        searchViewModel.searchResults.observe(viewLifecycleOwner){
-            searchAdapter.items.addAll(it)
-            searchAdapter.notifyDataSetChanged()
+        searchViewModel.apply {
+            searchResults.observe(viewLifecycleOwner){
+                searchAdapter.items.clear()
+                searchAdapter.items.addAll(it)
+                searchAdapter.notifyDataSetChanged()
+                isLoading.observe(viewLifecycleOwner){isLoading ->
+                    binding.searchLoading.visibility = if(isLoading) View.VISIBLE else View.GONE
+                    search_loading = !isLoading
+                }
+        }
         }
     }
 
@@ -67,13 +75,17 @@ class SearchFragment : Fragment() {
         searchAdapter = SearchFragmentAdapter(requireContext())
 
         binding.searchRecyclerView.apply {
-            layoutManager = GridLayoutManager(requireContext(),2)
+            layoutManager = GridLayoutManager(context,2)
             adapter = searchAdapter
+            addOnScrollListener(SearchScrollListener(searchViewModel,this@SearchFragment))
             setHasFixedSize(true)
         }
     }
 
-    private fun setupListeners(q:String,videoCategoryId:String){
-        searchViewModel.SearchServerResults(q,videoCategoryId)
+    private fun setupListeners(){
+        Log.d("YouTubeProjects","검색 호출")
+        searchQuery?.let {
+            searchViewModel.SearchServerResults(searchQuery!!,"2",searchViewModel.currentResults)
+        }
     }
 }
