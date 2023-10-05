aa1d0d66a4e9704f34f461399a628105fc0eb432
seongssu <loveforggong@gmail.com>
2023-10-06 05:08:42 +0900

    [FEAT] : ⚙️ 검색결과가 없을때 "검색 결과가 없습니다." textview 화면에 표시

--- app/src/main/java/com/android/youtubeproject/fragment/searchfragment/SearchFragmentAdapter.kt
+++ app/src/main/java/com/android/youtubeproject/fragment/searchfragment/SearchFragmentAdapter.kt
@@ -4,7 +4,9 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.android.youtubeproject.Constants
 import com.android.youtubeproject.api.model.ChannelModel
+import com.android.youtubeproject.databinding.EmptyViewBinding
 import com.android.youtubeproject.databinding.FavoritesitemsBinding
 import com.android.youtubeproject.`interface`.ItemClick
 import com.bumptech.glide.Glide
@@ -27,18 +29,35 @@ class SearchFragmentAdapter(private val context:Context):RecyclerView.Adapter<Re
             }
         }
     }
+    inner class emptyViewHolder(private val binding: EmptyViewBinding):RecyclerView.ViewHolder
+        (binding.root){
+        fun bind(){
+            binding.emptyTextView.text = "검색 결과가 없습니다."
+        }
+    }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        val binding = FavoritesitemsBinding.inflate(LayoutInflater.from(parent.context),parent,false)
-        return SearchViewHolder(binding)
+        return if(viewType == Constants.EMPTY_TYPE){
+            val binding = EmptyViewBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+            emptyViewHolder(binding)
+        }else{
+            val binding = FavoritesitemsBinding.inflate(LayoutInflater.from(parent.context),
+                parent,false)
+            SearchViewHolder(binding)
+        }
     }
 
     override fun getItemCount(): Int {
-        return items.size
+        return if (items.isEmpty()) 1 else items.size
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        holder as SearchViewHolder
-        val items = items[position]
-        holder.bind(items)
+        if(getItemViewType(position) == Constants.SEARCH_TYPE){
+            holder as SearchViewHolder
+            val items = items[position]
+            holder.bind(items)
+        }
+    }
+    override fun getItemViewType(position: Int): Int {
+        return if(items.isEmpty()) Constants.EMPTY_TYPE else Constants.SEARCH_TYPE
     }
 }
